a3cf7afb92974a6305f31bfff9ef349fefa8e98d
bommbomm34 <bommbomm34@perhof.org>
2026-01-24 11:37:24 +0100

    Suit AgentClient (theorethically) for parallel requests

--- composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/api/AgentClient.kt
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/api/AgentClient.kt
@@ -6,24 +6,29 @@ import io.github.bommbomm34.intervirt.result
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.plugins.websocket.*
 import io.ktor.http.*
+import io.ktor.serialization.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import kotlinx.serialization.json.Json
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.launch
 import java.io.File
+import java.util.concurrent.ConcurrentHashMap
 
 class AgentClient {
     private val logger = KotlinLogging.logger { }
     private lateinit var session: DefaultClientWebSocketSession
-    private var sessionLock = Mutex()
+    private val requests = ConcurrentHashMap<String, MutableSharedFlow<ResponseBody>>()
 
-    suspend fun initConnection(): Result<Unit> {
-        try {
-            if (!this::session.isInitialized) {
+    suspend fun listen(): Result<Unit> {
+        if (!this::session.isInitialized) {
+            val result = runCatching {
                 session = client.webSocketSession(
                     method = HttpMethod.Get,
                     host = "localhost",
@@ -31,10 +36,19 @@ class AgentClient {
                     path = "containerManagement"
                 )
             }
-            return Result.success(Unit)
-        } catch (e: Exception) {
-            return Result.failure(e)
+            CoroutineScope(Dispatchers.IO).launch {
+                while (true) {
+                    try {
+                        val response = session.receiveDeserialized<ResponseBody>()
+                        requests[response.refID]!!.emit(response)
+                    } catch (e: WebsocketDeserializeException) {
+                        if (e.frame is Frame.Close) break
+                    }
+                }
+            }
+            return result
         }
+        return Result.success(Unit)
     }
 
     suspend fun addContainer(
@@ -106,7 +120,7 @@ class AgentClient {
     suspend fun reboot(): Result<Unit> = justSend("reboot".commandBody())
 
     suspend fun getVersion(): Result<String> {
-        val response = send<VersionResponseBody>("version".commandBody())
+        val response = send<ResponseBody.Version>("version".commandBody())
         response
             .onSuccess {
                 return Result.success(it.firstOrNull()!!.version)
@@ -121,7 +135,7 @@ class AgentClient {
         flowSend(RequestBody.RunCommand(id, command))
 
     private suspend fun justSend(body: RequestBody): Result<Unit> {
-        val response = send<ResponseBody>(body)
+        val response = send<ResponseBody.General>(body)
         response
             .onSuccess {
                 return it.firstOrNull()?.exception()?.result() ?: Result.success(Unit)
@@ -134,7 +148,7 @@ class AgentClient {
 
     private fun flowSend(body: RequestBody): Flow<ResultProgress<Unit>> = flow {
         var failed = false
-        send<ResponseBody>(body)
+        send<ResponseBody.General>(body)
             .onSuccess { flow ->
                 flow.collect {
                     if (it.code != 0) {
@@ -152,28 +166,18 @@ class AgentClient {
         if (!failed) emit(ResultProgress.success(Unit))
     }
 
-    private suspend inline fun <reified T> send(body: RequestBody): Result<Flow<T>> {
-        val flow = flow {
-            val requestMessage = Json.encodeToString(body)
-            logger.debug { "CLIENT: $requestMessage" }
-            session.send(Frame.Text(requestMessage))
-            while (true) {
-                val responseMessage = (session.incoming.receive() as Frame.Text).readText()
-                val data = Json.decodeFromString<T>(responseMessage)
-                logger.debug { "AGENT: $responseMessage" }
-                emit(data)
-                if (!(data is ResponseBody && data.code == -1)) {
-                    logger.debug { "AGENT END" }
-                    break
-                }
-            }
-        }
-        sessionLock.withLock {
-            logger.debug { "Checking connection with agent" }
-            initConnection().fold(
-                onSuccess = { return Result.success(flow) },
-                onFailure = { return Result.failure(it) }
-            )
-        }
+    @Suppress("UNCHECKED_CAST")
+    private suspend fun <T : ResponseBody> send(body: RequestBody): Result<Flow<T>> {
+        logger.debug { "Checking connection with agent" }
+        listen().fold(
+            onSuccess = {
+                requests[body.uuid] = MutableSharedFlow()
+                session.sendSerialized(body)
+                return Result.success(requests[body.uuid]!!.onCompletion {
+                    requests.remove(body.uuid)
+                }.map { it as T })
+            },
+            onFailure = { return Result.failure(it) }
+        )
     }
 }
--- composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/data/ResponseBody.kt
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/data/ResponseBody.kt
@@ -1,34 +1,44 @@
 package io.github.bommbomm34.intervirt.data
 
 import io.github.bommbomm34.intervirt.exceptions.*
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ResponseBody(
-    val error: String? = null,
-    val code: Int = 0,
-    val progress: Float? = null,
-    val output: String? = null
-) {
-    fun exception(): Exception? {
-        return when (code) {
-            1 -> UndefinedError(error!!)
-            2 -> UnknownError()
-            3 -> OperationAlreadyPerformedError()
-            4 -> OSError(error!!)
-            5 -> ContainerExecutionException(error!!)
-            6 -> NotFoundError(error!!)
-            0 -> null
-            else -> error("Invalid status code $code")
+sealed class ResponseBody(){
+    abstract val refID: String
+    @SerialName("General")
+    @Serializable
+    data class General(
+        override val refID: String,
+        val error: String? = null,
+        val code: Int = 0,
+        val progress: Float? = null,
+        val output: String? = null,
+    ) : ResponseBody() {
+        fun exception(): Exception? {
+            return when (code) {
+                1 -> UndefinedError(error!!)
+                2 -> UnknownError()
+                3 -> OperationAlreadyPerformedError()
+                4 -> OSError(error!!)
+                5 -> ContainerExecutionException(error!!)
+                6 -> NotFoundError(error!!)
+                0 -> null
+                else -> error("Invalid status code $code")
+            }
         }
     }
+
+    @SerialName("Version")
+    @Serializable
+    data class Version(
+        override val refID: String,
+        val version: String,
+        val canRunCommands: Boolean
+    ) : ResponseBody()
 }
 
-@Serializable
-data class VersionResponseBody(
-    val version: String,
-    val canRunCommands: Boolean
-)
 
 //    data class ContainerInfo (
 //        val id: String,
