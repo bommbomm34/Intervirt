433521735fe0c5b6fa0adc49b68a62b6e65605cb
bommbomm34 <bommbomm34@perhof.org>
2026-01-29 17:56:11 +0100

    Implement ContainerTtyConnector

--- composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/api/ContainerTtyConnector.kt
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/api/ContainerTtyConnector.kt
@@ -6,13 +6,22 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.io.PrintWriter
+import kotlin.math.min
 
 class ContainerTtyConnector(
     private val session: RemoteContainerSession
 ) : TtyConnector {
 
-    override fun read(buf: CharArray?, offset: Int, length: Int): Int {
-        TODO("Not yet implemented")
+    override fun read(buf: CharArray, offset: Int, length: Int): Int {
+        val bytes = session.read(length)
+        val charArray = bytes.decodeToString().toCharArray()
+        charArray.forEachIndexed { i, char ->
+            buf[i + offset] = char
+        }
+        return min(charArray.size, buf.size)
     }
 
     override fun write(bytes: ByteArray?): Unit = runBlocking {
--- composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/data/RemoteContainerSession.kt
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/data/RemoteContainerSession.kt
@@ -1,34 +1,40 @@
 package io.github.bommbomm34.intervirt.data
 
-import androidx.compose.ui.util.fastSumBy
-import io.ktor.client.plugins.websocket.DefaultClientWebSocketSession
-import io.ktor.websocket.close
-import io.ktor.websocket.send
+import io.ktor.client.plugins.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.getOrElse
 import kotlinx.coroutines.isActive
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 
 data class RemoteContainerSession(
     val id: String,
-    private val websocket: DefaultClientWebSocketSession,
+    private val websocket: DefaultClientWebSocketSession
 ) {
-    // TODO: Fill byteStack
-    private val byteStack = mutableListOf<ByteArray>()
-    private val byteStackLock = Mutex()
 
     suspend fun write(bytes: ByteArray): Result<Unit> = runCatching {
         websocket.send(bytes)
     }
 
-    suspend fun read(): ByteArray {
-        byteStackLock.withLock {
-            val byteArray = byteStack.flatMap { it.toList() }.toByteArray()
-            byteStack.clear()
-            return byteArray
-        }
+    fun read(length: Int): ByteArray {
+        val byteArray = websocket.incoming
+            .receive(length)
+            .flatMap { it.toList() }
+            .toByteArray()
+        return byteArray
     }
 
     suspend fun close() = websocket.close()
-
     fun isConnected(): Boolean = websocket.isActive
+
+    private fun ReceiveChannel<Frame>.receive(length: Int): List<ByteArray> {
+        val list = mutableListOf<ByteArray>()
+        var lengthOfBytes = 0
+        while (true) {
+            val frame = tryReceive().getOrElse { return list }
+            val bytes = (frame as Frame.Binary).readBytes()
+            lengthOfBytes += bytes.size
+            if (lengthOfBytes < length) list.add(bytes) else return list
+        }
+    }
 }
