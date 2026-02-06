49a43570604e7891b4c7923088a0d5e1f62edb10
bommbomm34 <bommbomm34@perhof.org>
2026-02-06 20:30:49 +0100

    Add SimpleTable
    Simplifies the compose-table API

---
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/components/SimpleTable.kt
@@ -0,0 +1,52 @@
+package io.github.bommbomm34.intervirt.gui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import io.github.bommbomm34.intervirt.data.AppEnv
+import io.github.bommbomm34.intervirt.isDarkMode
+import io.github.windedge.table.DataTable
+import org.koin.compose.koinInject
+
+@Composable
+fun SimpleTable(
+    headers: List<String>,
+    content: List<List<Any>>
+){
+    SelectionContainer {
+        DataTable(
+            columns = {
+                headerBackground {
+                    Box(Modifier.background(MaterialTheme.colorScheme.onBackground))
+                }
+                headers.forEach {
+                    column { VisibleText(it, true) }
+                }
+            }
+        ){
+            content.forEach { row ->
+                row {
+                    row.forEach {
+                        cell { VisibleText(it) }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun VisibleText(text: Any, bold: Boolean = false){
+    val appEnv = koinInject<AppEnv>()
+    Text(
+        text = text.toString(),
+        color = if (appEnv.isDarkMode()) Color.White else Color.Black,
+        fontWeight = if (bold) FontWeight.ExtraBold else null
+    )
+}
--- composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/intervirtos/dns/DnsRecordsTable.kt
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/intervirtos/dns/DnsRecordsTable.kt
@@ -1,60 +1,25 @@
 package io.github.bommbomm34.intervirt.gui.intervirtos.dns
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import intervirt.composeapp.generated.resources.Res
 import intervirt.composeapp.generated.resources.class_name
 import intervirt.composeapp.generated.resources.data
 import intervirt.composeapp.generated.resources.name
 import intervirt.composeapp.generated.resources.type
-import io.github.bommbomm34.intervirt.data.AppEnv
 import io.github.bommbomm34.intervirt.data.dns.DnsRecord
-import io.github.bommbomm34.intervirt.isDarkMode
-import io.github.windedge.table.DataTable
+import io.github.bommbomm34.intervirt.gui.components.SimpleTable
 import org.jetbrains.compose.resources.stringResource
-import org.koin.compose.koinInject
 
 @Composable
 fun DnsRecordsTable(records: List<DnsRecord>){
-    SelectionContainer {
-        DataTable(
-            columns = {
-                headerBackground {
-                    Box(Modifier.background(MaterialTheme.colorScheme.onBackground))
-                }
-                column { VisibleText(stringResource(Res.string.name), true) }
-                column { VisibleText("TTL", true) }
-                column { VisibleText(stringResource(Res.string.class_name), true) }
-                column { VisibleText(stringResource(Res.string.type), true) }
-                column { VisibleText(stringResource(Res.string.data), true) }
-            }
-        ){
-            records.forEach {
-                row {
-                    cell { VisibleText(it.name) }
-                    cell { VisibleText(it.ttl) }
-                    cell { VisibleText(it.dnsClass) }
-                    cell { VisibleText(it.type) }
-                    cell { VisibleText(it.data) }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-private fun VisibleText(text: Any, bold: Boolean = false){
-    val appEnv = koinInject<AppEnv>()
-    Text(
-        text = text.toString(),
-        color = if (appEnv.isDarkMode()) Color.White else Color.Black,
-        fontWeight = if (bold) FontWeight.ExtraBold else null
+    SimpleTable(
+        headers = listOf(
+            stringResource(Res.string.name),
+            "TTL",
+            stringResource(Res.string.class_name),
+            stringResource(Res.string.type),
+            stringResource(Res.string.data)
+        ),
+        content = records.map { listOf(it.name, it.ttl, it.dnsClass, it.type, it.data) }
     )
 }
