61dfc0667600a9da695d01733e8642010c6ddd33
bommbomm34 <bommbomm34@perhof.org>
2026-02-08 20:03:36 +0100

    Add CustomTable

--- composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/components/SimpleTable.kt
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/components/SimpleTable.kt
@@ -1,18 +1,6 @@
 package io.github.bommbomm34.intervirt.gui.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import io.github.bommbomm34.intervirt.data.AppEnv
-import io.github.bommbomm34.intervirt.isDarkMode
-import io.github.windedge.table.DataTable
-import org.koin.compose.koinInject
 
 @Composable
 fun SimpleTable(
@@ -20,37 +8,13 @@ fun SimpleTable(
     content: List<List<Any>>,
     customElements: List<@Composable () -> Unit> = emptyList()
 ){
-    SelectionContainer {
-        DataTable(
-            columns = {
-                headerBackground {
-                    Box(Modifier.background(MaterialTheme.colorScheme.onBackground))
-                }
-                headers.forEach {
-                    column { VisibleText(it, true) }
-                }
-            }
-        ){
-            content.forEachIndexed { i, row ->
-                row {
-                    row.forEach {
-                        cell {
-                            VisibleText(it)
-                        }
-                    }
-                    customElements.getOrNull(i)?.let { cell { it() } }
-                }
+    CustomTable(
+        headers = headers,
+        content = content.mapIndexed { i, row ->
+            buildList {
+                addAll(row.map { { VisibleText(it) } })
+                customElements.getOrNull(i)?.let { element -> add(element) }
             }
         }
-    }
-}
-
-@Composable
-private fun VisibleText(text: Any, bold: Boolean = false){
-    val appEnv = koinInject<AppEnv>()
-    Text(
-        text = text.toString(),
-        color = if (appEnv.isDarkMode()) Color.White else Color.Black,
-        fontWeight = if (bold) FontWeight.ExtraBold else null
     )
 }
