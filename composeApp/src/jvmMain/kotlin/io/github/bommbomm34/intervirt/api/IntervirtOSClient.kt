265d4ce1c31bde5cc7d13c9668f691f55adf3073
bommbomm34 <bommbomm34@perhof.org>
2026-02-11 20:18:45 +0100

    Implement AddMailUserView

--- composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/api/IntervirtOSClient.kt
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/api/IntervirtOSClient.kt
@@ -11,7 +11,6 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.serialization.json.Json
 import kotlin.io.path.notExists
 import kotlin.io.path.readLines
-import kotlin.io.path.readText
 import kotlin.io.path.writeText
 
 private val json = Json {
@@ -40,7 +39,7 @@ class IntervirtOSClient(
             onSuccess = { flow ->
                 val (output, statusCode) = flow.getCommandResult()
                 logger.debug { "Received during DNS lookup:\n$output" }
-                if (statusCode == 0){
+                if (statusCode == 0) {
                     val resolverOutput = json.decodeFromString<DnsResolverOutput>(output)
                     resolverOutput.responses
                         .getOrNull(0)
@@ -99,18 +98,16 @@ class IntervirtOSClient(
                 if (statusCode != 0) {
                     logger.error { "Failed to list mail users: $output" }
                     Result.failure(ContainerExecutionException(output))
-                } else {
-                    runCatching {
-                        output.lines().map {
-                            val name = it.split(":")[3]
-                            val addressFile = ioClient.getPath("/home/$name/.intervirt_mail_address")
-                            if (addressFile.notExists()) {
-                                val error = "Missing mail address file for user $name"
-                                logger.error { error }
-                                error(error)
-                            }
-                            MailUser(name, addressFile.readLines()[0])
+                } else runCatching {
+                    output.lines().map {
+                        val name = it.split(":")[3]
+                        val addressFile = ioClient.getPath("/home/$name/.intervirt_mail_address")
+                        if (addressFile.notExists()) {
+                            val error = "Missing mail address file for user $name"
+                            logger.error { error }
+                            error(error)
                         }
+                        MailUser(name, addressFile.readLines()[0])
                     }
                 }
             },
@@ -127,6 +124,29 @@ class IntervirtOSClient(
                     logger.error { "Error during removing user $name: $output" }
                     Result.failure(ContainerExecutionException(output))
                 } else Result.success(Unit)
+            },
+            onFailure = { Result.failure(it) }
+        )
+    }
+
+    // TODO: Accept password in a more secure way
+    suspend fun addMailUser(user: MailUser, password: String): Result<Unit> {
+        logger.debug { "Add mail user ${user.username} with email ${user.address}" }
+        val command = listOf(
+            "useradd",
+            "-m",
+            "-p", "$(openssl passwd -6 \"$password\")",
+            user.username
+        )
+        return ioClient.exec(command).fold(
+            onSuccess = { flow ->
+                val (output, statusCode) = flow.getCommandResult()
+                if (statusCode != 0) {
+                    logger.error { "Error during adding mail user ${user.username}: $output" }
+                    Result.failure(ContainerExecutionException(output))
+                } else runCatching {
+                    ioClient.getPath("/home/${user.username}/.intervirt_mail_address").writeText(user.address)
+                }
             },
             onFailure = { Result.failure(it) }
         )
--- composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/intervirtos/mail/server/AddMailUserView.kt
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/intervirtos/mail/server/AddMailUserView.kt
@@ -1,11 +1,59 @@
 package io.github.bommbomm34.intervirt.gui.intervirtos.mail.server
 
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import intervirt.composeapp.generated.resources.Res
+import intervirt.composeapp.generated.resources.add_user
+import intervirt.composeapp.generated.resources.email_address
+import intervirt.composeapp.generated.resources.username
 import io.github.bommbomm34.intervirt.api.IntervirtOSClient
+import io.github.bommbomm34.intervirt.data.MailUser
+import io.github.bommbomm34.intervirt.gui.components.CenterRow
+import io.github.bommbomm34.intervirt.gui.components.GeneralSpacer
+import io.github.bommbomm34.intervirt.gui.components.PasswordTextField
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun AddMailUserView(
     osClient: IntervirtOSClient
 ){
-    // TODO: Implement it
+    // TODO: Save the password more secure
+    val scope = rememberCoroutineScope()
+    var username by remember { mutableStateOf("") }
+    var emailAddress by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    CenterRow {
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text(stringResource(Res.string.username)) }
+        )
+        GeneralSpacer()
+        OutlinedTextField(
+            value = emailAddress,
+            onValueChange = { emailAddress = it },
+            label = { Text(stringResource(Res.string.email_address)) }
+        )
+        GeneralSpacer()
+        PasswordTextField(
+            value = password,
+            onValueChange = { password = it }
+        )
+        GeneralSpacer()
+        Button(
+            onClick = {
+                scope.launch {
+                    osClient.addMailUser(MailUser(username, emailAddress), password).getOrThrow()
+                    username = ""
+                    emailAddress = ""
+                    password = ""
+                }
+            }
+        ){
+            Text(stringResource(Res.string.add_user))
+        }
+    }
 }
