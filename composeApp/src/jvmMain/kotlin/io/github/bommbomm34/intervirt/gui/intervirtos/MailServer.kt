507fe269285fc6aab2cece6efe6fafa8fda3a5d8
bommbomm34 <bommbomm34@perhof.org>
2026-02-11 19:36:26 +0100

    Implement incomplete MailServer

--- composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/intervirtos/MailServer.kt
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/intervirtos/MailServer.kt
@@ -1,2 +1,36 @@
 package io.github.bommbomm34.intervirt.gui.intervirtos
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import io.github.bommbomm34.intervirt.api.IntervirtOSClient
+import io.github.bommbomm34.intervirt.gui.components.AlignedBox
+import io.github.bommbomm34.intervirt.gui.components.GeneralSpacer
+import io.github.bommbomm34.intervirt.gui.components.MultipleAnimatedVisibility
+import io.github.bommbomm34.intervirt.gui.intervirtos.components.NamedSystemServiceView
+import io.github.bommbomm34.intervirt.gui.intervirtos.mail.server.MailServerUserManager
+
+@Composable
+fun MailServer(
+    osClient: IntervirtOSClient
+){
+    // Controls for server
+    AlignedBox(Alignment.TopEnd){
+        NamedSystemServiceView(
+            displayName = "SMTP",
+            serviceName = "postfix",
+            serviceManager = osClient.serviceManager
+        )
+        GeneralSpacer()
+        NamedSystemServiceView(
+            displayName = "IMAP",
+            serviceName = "dovecot",
+            serviceManager = osClient.serviceManager
+        )
+    }
+    GeneralSpacer()
+    MailServerUserManager(osClient)
+}
---
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/intervirtos/mail/server/MailServerUserManager.kt
@@ -0,0 +1,9 @@
+package io.github.bommbomm34.intervirt.gui.intervirtos.mail.server
+
+import androidx.compose.runtime.Composable
+import io.github.bommbomm34.intervirt.api.IntervirtOSClient
+
+@Composable
+fun MailServerUserManager(osClient: IntervirtOSClient){
+
+}
