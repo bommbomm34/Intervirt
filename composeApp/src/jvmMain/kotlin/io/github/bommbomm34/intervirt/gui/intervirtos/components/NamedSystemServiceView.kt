c9a771dbe216fa66f7a4d5fbee6d12ba7b841a51
bommbomm34 <bommbomm34@perhof.org>
2026-02-11 19:31:55 +0100

    Add NamedSystemServiceView

---
+++ composeApp/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/intervirtos/components/NamedSystemServiceView.kt
@@ -0,0 +1,20 @@
+package io.github.bommbomm34.intervirt.gui.intervirtos.components
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import io.github.bommbomm34.intervirt.api.SystemServiceManager
+import io.github.bommbomm34.intervirt.gui.components.CenterRow
+import io.github.bommbomm34.intervirt.gui.components.GeneralSpacer
+
+@Composable
+fun NamedSystemServiceView(
+    displayName: String,
+    serviceName: String,
+    serviceManager: SystemServiceManager
+) {
+    CenterRow {
+        Text(displayName)
+        GeneralSpacer()
+        SystemServiceView(serviceName, serviceManager)
+    }
+}
