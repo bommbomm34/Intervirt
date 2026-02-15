0b8ccf9ff82c229f054da8163d916d73c75a466e
bommbomm34 <bommbomm34@perhof.org>
2026-02-15 09:43:49 +0100

    Add experimental NativeMailSender

--- mail/src/main/rust/lib.rs
+++ mail/src/main/rust/lib.rs
@@ -1,28 +1,90 @@
-/// This exports this Rust function to the Kotlin side.
-#[uniffi::export]
-fn add(lhs: i32, rhs: i32) -> i32 {
-    lhs + rhs
+use lettre::message::{Mailbox, header::ContentType};
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
+
+#[derive(uniffi::Record)]
+struct Address {
+    host: String,
+    port: u16,
 }
 
-/// This exports this Rust type to the Kotlin side.
 #[derive(uniffi::Object)]
-struct Greeter {
-    greeting: String
+struct NativeMailSender {
+    mailer: SmtpTransport,
+}
+
+#[derive(uniffi::Enum)]
+enum MailBodyType {
+    TEXT,
+    HTML,
+}
+
+#[derive(uniffi::Record)]
+struct Mail {
+    sender: String,
+    receiver: String,
+    subject: String,
+    body_type: MailBodyType,
+    body: String,
+}
+
+#[derive(uniffi::Record)]
+struct SmtpCredentials {
+    username: String,
+    password: String,
 }
 
 #[uniffi::export]
-impl Greeter {
-   /// Define a constructor to be used on the Kotlin side.
-   #[uniffi::constructor]
-   fn new(greeting: String) -> Self {
-      Self { greeting }
-   }
+impl NativeMailSender {
+    #[uniffi::constructor]
+    fn new(
+        host: Address,
+        credentials: Option<SmtpCredentials>,
+        _proxy: Option<Address>,
+    ) -> Result<Self, String> {
+        let mailer_builder = SmtpTransport::relay(host.host.as_str());
+        match mailer_builder {
+            Ok(mailer_builder) => match credentials {
+                Some(creds) => {
+                    let creds = Credentials::new(creds.username, creds.password);
+                    Ok(NativeMailSender {
+                        mailer: mailer_builder.credentials(creds.to_owned()).build(),
+                    })
+                }
+                None => Ok(NativeMailSender {
+                    mailer: mailer_builder.build(),
+                }),
+            },
+            Err(error) => Err(error.to_string()),
+        }
+    }
 
-   /// Define a method to be used on the Kotlin side.
-   fn greet(&self, name: String) -> String {
-      format!("{}, {name}!", self.greeting)
-   }
+    fn send_mail(&self, mail: &Mail) -> Result<(), String> {
+        let email = Message::builder()
+            .from(Mailbox::new(
+                Some(mail.sender.clone()),
+                mail.sender.parse().unwrap(),
+            ))
+            .to(Mailbox::new(
+                Some(mail.receiver.clone()),
+                mail.receiver.parse().unwrap(),
+            ))
+            .subject(mail.subject.clone())
+            .header(to_content_type(&mail.body_type))
+            .body(mail.body.clone())
+            .unwrap();
+        self.mailer
+            .send(&email)
+            .map(|_| ())
+            .map_err(|err| err.to_string())
+    }
 }
 
-// This generates extra Rust code required by UniFFI.
+fn to_content_type(body_type: &MailBodyType) -> ContentType {
+    match body_type {
+        MailBodyType::TEXT => ContentType::TEXT_PLAIN,
+        MailBodyType::HTML => ContentType::TEXT_HTML,
+    }
+}
+
 uniffi::setup_scaffolding!();
