f1257fa9bfc25cb44b3a94b34094a28b96763081
bommbomm34 <bommbomm34@perhof.org>
2026-02-17 16:55:25 +0100

    Refactor AppEnv with delegation properties

--- core/src/main/kotlin/io/github/bommbomm34/intervirt/core/api/Preferences.kt
+++ core/src/main/kotlin/io/github/bommbomm34/intervirt/core/api/Preferences.kt
@@ -16,10 +16,6 @@ class Preferences {
     private val logger = KotlinLogging.logger {  }
     private val data = mutableMapOf<String, String>()
     private val dataFile: Path = File(System.getProperty("user.home") + File.separator + ".intervirt.config.json").toPath()
-    private val defaultQemuZipUrl = when (getOS()){
-        OS.WINDOWS -> "https://cdn.perhof.org/bommbomm34/qemu/windows-portable.zip"
-        OS.LINUX -> "https://cdn.perhof.org/bommbomm34/qemu/linux-portable.zip"
-    }
 
     init { load() }
 
@@ -37,43 +33,7 @@ class Preferences {
         flush()
     }
 
-    fun getAppEnv() = AppEnv(
-        debugEnabled = env("DEBUG_ENABLED").toBoolean(),
-        agentTimeout = env("AGENT_TIMEOUT")?.toLong() ?: 30000L,
-        qemuMonitorTimeout = env("QEMU_MONITOR_TIMEOUT")?.toLong() ?: 5000L,
-        agentPort = env("AGENT_PORT")?.toInt() ?: 55436,
-        vmShutdownTimeout = env("VM_SHUTDOWN_TIMEOUT")?.toLong() ?: 30000L,
-        vmRam = env("VM_RAM")?.toInt() ?: 2048,
-        vmCpu = env("VM_CPU")?.toInt() ?: 1,
-        vmEnableKvm = env("VM_ENABLE_KVM")?.toBoolean() ?: false,
-        dataDir = File(env("DATA_DIR") ?: (System.getProperty("user.home") + File.separator + "Intervirt")),
-        darkMode = env("DARK_MODE")?.toBoolean(),
-        tooltipFontSize = env("TOOLTIP_FONT_SIZE")?.toInt() ?: 12,
-        connectionStrokeWidth = env("CONNECTION_STROKE_WIDTH")?.toFloat() ?: 10f,
-        deviceConnectionColor = env("DEVICE_CONNECTION_COLOR")?.toLong(16) ?: 0xFF9CCC65,
-        zoomSpeed = env("ZOOM_SPEED")?.toFloat() ?: 0.1f,
-        deviceSize = env("DEVICE_SIZE")?.toInt() ?: 100,
-        osIconSize = env("OS_ICON_SIZE")?.toInt() ?: 128,
-        suggestedFilename = env("SUGGESTED_FILENAME") ?: "MyIntervirtProject",
-        language = env("LANGUAGE")?.let { Locale.forLanguageTag(it) } ?: Locale.getDefault() ?: Locale.US,
-        enableAgent = env("ENABLE_AGENT")?.toBooleanStrictOrNull() ?: true,
-        qemuMonitorPort = env("QEMU_MONITOR_PORT")?.toInt() ?: 55437,
-        titleFontSize = env("TITLE_FONT_SIZE")?.toInt() ?: 48,
-        appIconSize = env("APP_ICON_SIZE")?.toInt(),
-        defaultDnsServer = env("DEFAULT_DNS_SERVER") ?: "9.9.9.9",
-        pseudoMode = env("PSEUDO_MODE").toBoolean(),
-        enableJavaScript = env("ENABLE_JAVASCRIPT")?.toBoolean() ?: true,
-        virtualContainerIO = env("VIRTUAL_CONTAINER_IO").toBoolean(),
-        vmDiskUrl = env("VM_DISK_URL") ?: "https://cdn.perhof.org/bommbomm34/intervirt/alpine-disk.qcow2",
-        vmDiskHashUrl = env("VM_DISK_HASH_URL") ?: "https://cdn.perhof.org/bommbomm34/intervirt/alpine-disk.qcow2.sha256",
-        qemuZipUrl = env("QEMU_ZIP_URL") ?: defaultQemuZipUrl,
-        qemuZipHashUrl = env("QEMU_ZIP_HASH_URL") ?: "$defaultQemuZipUrl.sha256",
-        agentWebSocketTimeout = env("AGENT_WEBSOCKET_TIMEOUT")?.toLong() ?: 10_000L,
-        mailUsername = env("MAIL_USERNAME") ?: "",
-        mailPassword = env("MAIL_PASSWORD") ?: "",
-        smtpServerAddress = env("SMTP_SERVER_ADDRESS") ?: "",
-        imapServerAddress = env("IMAP_SERVER_ADDRESS") ?: "",
-    )
+    fun getAppEnv() = AppEnv(::env, ::saveString)
     
     fun env(name: String): String? = System.getenv("INTERVIRT_$name") ?: loadString(name)
 
--- core/src/main/kotlin/io/github/bommbomm34/intervirt/core/data/AppEnv.kt
+++ core/src/main/kotlin/io/github/bommbomm34/intervirt/core/data/AppEnv.kt
@@ -2,41 +2,172 @@ package io.github.bommbomm34.intervirt.core.data
 
 import java.io.File
 import java.util.*
+import kotlin.reflect.KProperty
 
 data class AppEnv(
-    val debugEnabled: Boolean,
-    val agentTimeout: Long,
-    val qemuMonitorTimeout: Long,
-    val agentPort: Int,
-    val vmShutdownTimeout: Long,
-    val vmRam: Int,
-    val vmCpu: Int,
-    val vmEnableKvm: Boolean,
-    val dataDir: File,
-    val darkMode: Boolean?,
-    val tooltipFontSize: Int,
-    val connectionStrokeWidth: Float,
-    val deviceConnectionColor: Long,
-    val zoomSpeed: Float,
-    val deviceSize: Int,
-    val osIconSize: Int,
-    val suggestedFilename: String,
-    val language: Locale,
-    val enableAgent: Boolean,
-    val qemuMonitorPort: Int,
-    val titleFontSize: Int,
-    val appIconSize: Int?,
-    val defaultDnsServer: String,
-    val pseudoMode: Boolean,
-    val enableJavaScript: Boolean,
-    val virtualContainerIO: Boolean,
-    val vmDiskUrl: String,
-    val vmDiskHashUrl: String,
-    val qemuZipUrl: String,
-    val qemuZipHashUrl: String,
-    val agentWebSocketTimeout: Long,
-    val mailUsername: String,
-    val mailPassword: String,
-    val smtpServerAddress: String,
-    val imapServerAddress: String
-)
+    private val env: (String) -> String?,
+    private val save: (String, String) -> Unit,
+    private val custom: AppEnv.() -> Unit = {}
+) {
+    private val defaultQemuZipUrl = when (getOS()) {
+        OS.WINDOWS -> "https://cdn.perhof.org/bommbomm34/qemu/windows-portable.zip"
+        OS.LINUX -> "https://cdn.perhof.org/bommbomm34/qemu/linux-portable.zip"
+    }
+
+    val debugEnabled: Boolean by EnvDelegate("DEBUG_ENABLED", save) {
+        env(it).toBoolean()
+    }
+
+    val agentTimeout: Long by EnvDelegate("AGENT_TIMEOUT", save) {
+        env(it)?.toLong() ?: 30_000L
+    }
+
+    val qemuMonitorTimeout: Long by EnvDelegate("QEMU_MONITOR_TIMEOUT", save) {
+        env(it)?.toLong() ?: 5_000L
+    }
+
+    val agentPort: Int by EnvDelegate("AGENT_PORT", save) {
+        env(it)?.toInt() ?: 55436
+    }
+
+    val vmShutdownTimeout: Long by EnvDelegate("VM_SHUTDOWN_TIMEOUT", save) {
+        env(it)?.toLong() ?: 30_000L
+    }
+
+    val vmRam: Int by EnvDelegate("VM_RAM", save) {
+        env(it)?.toInt() ?: 2048
+    }
+
+    val vmCpu: Int by EnvDelegate("VM_CPU", save) {
+        env(it)?.toInt() ?: 1
+    }
+
+    val vmEnableKvm: Boolean by EnvDelegate("VM_ENABLE_KVM", save) {
+        env(it)?.toBoolean() ?: false
+    }
+
+    val dataDir: File by EnvDelegate("DATA_DIR", save) {
+        File(env(it) ?: "${System.getProperty("user.home")}${File.separator}Intervirt")
+    }
+
+    val darkMode: Boolean? by EnvDelegate("DARK_MODE", save) {
+        env(it)?.toBoolean()
+    }
+
+    val tooltipFontSize: Int by EnvDelegate("TOOLTIP_FONT_SIZE", save) {
+        env(it)?.toInt() ?: 12
+    }
+
+    val connectionStrokeWidth: Float by EnvDelegate("CONNECTION_STROKE_WIDTH", save) {
+        env(it)?.toFloat() ?: 10f
+    }
+
+    val deviceConnectionColor: Long by EnvDelegate("DEVICE_CONNECTION_COLOR", save) {
+        env(it)?.toLong(16) ?: 0xFF9CCC65
+    }
+
+    val zoomSpeed: Float by EnvDelegate("ZOOM_SPEED", save) {
+        env(it)?.toFloat() ?: 0.1f
+    }
+
+    val deviceSize: Int by EnvDelegate("DEVICE_SIZE", save) {
+        env(it)?.toInt() ?: 100
+    }
+
+    val osIconSize: Int by EnvDelegate("OS_ICON_SIZE", save) {
+        env(it)?.toInt() ?: 128
+    }
+
+    val suggestedFilename: String by EnvDelegate("SUGGESTED_FILENAME", save) {
+        env(it) ?: "MyIntervirtProject"
+    }
+
+    val language: Locale by EnvDelegate("LANGUAGE", save) {
+        env(it)?.let(Locale::forLanguageTag)
+            ?: Locale.getDefault()
+            ?: Locale.US
+    }
+
+    val enableAgent: Boolean by EnvDelegate("ENABLE_AGENT", save) {
+        env(it)?.toBooleanStrictOrNull() ?: true
+    }
+
+    val qemuMonitorPort: Int by EnvDelegate("QEMU_MONITOR_PORT", save) {
+        env(it)?.toInt() ?: 55_437
+    }
+
+    val titleFontSize: Int by EnvDelegate("TITLE_FONT_SIZE", save) {
+        env(it)?.toInt() ?: 48
+    }
+
+    val appIconSize: Int? by EnvDelegate("APP_ICON_SIZE", save) {
+        env(it)?.toInt()
+    }
+
+    val defaultDnsServer: String by EnvDelegate("DEFAULT_DNS_SERVER", save) {
+        env(it) ?: "9.9.9.9"
+    }
+
+    val pseudoMode: Boolean by EnvDelegate("PSEUDO_MODE", save) {
+        env(it).toBoolean()
+    }
+
+    val enableJavaScript: Boolean by EnvDelegate("ENABLE_JAVASCRIPT", save) {
+        env(it)?.toBoolean() ?: true
+    }
+
+    val virtualContainerIO: Boolean by EnvDelegate("VIRTUAL_CONTAINER_IO", save) {
+        env(it).toBoolean()
+    }
+
+    val vmDiskUrl: String by EnvDelegate("VM_DISK_URL", save) {
+        env(it) ?: "https://cdn.perhof.org/bommbomm34/intervirt/alpine-disk.qcow2"
+    }
+
+    val vmDiskHashUrl: String by EnvDelegate("VM_DISK_HASH_URL", save) {
+        env(it) ?: "https://cdn.perhof.org/bommbomm34/intervirt/alpine-disk.qcow2.sha256"
+    }
+
+    val qemuZipUrl: String by EnvDelegate("QEMU_ZIP_URL", save) {
+        env(it) ?: defaultQemuZipUrl
+    }
+
+    val qemuZipHashUrl: String by EnvDelegate("QEMU_ZIP_HASH_URL", save) {
+        env(it) ?: "$defaultQemuZipUrl.sha256"
+    }
+
+    val agentWebSocketTimeout: Long by EnvDelegate("AGENT_WEBSOCKET_TIMEOUT", save) {
+        env(it)?.toLong() ?: 10_000L
+    }
+
+    val mailUsername: String by EnvDelegate("MAIL_USERNAME", save) {
+        env(it) ?: ""
+    }
+
+    val mailPassword: String by EnvDelegate("MAIL_PASSWORD", save) {
+        env(it) ?: ""
+    }
+
+    val smtpServerAddress: String by EnvDelegate("SMTP_SERVER_ADDRESS", save) {
+        env(it) ?: ""
+    }
+
+    val imapServerAddress: String by EnvDelegate("IMAP_SERVER_ADDRESS", save) {
+        env(it) ?: ""
+    }
+}
+
+private class EnvDelegate<T>(
+    private val env: String,
+    private val save: (env: String, new: String) -> Unit,
+    producer: (env: String) -> T
+) {
+    private var value = producer(env)
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T = value
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        save(env, value.toString())
+        this.value = value
+    }
+}
