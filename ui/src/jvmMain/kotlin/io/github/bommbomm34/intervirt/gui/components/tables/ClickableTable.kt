1a939629c9967effafeb6dcac08730f17ea03c53
bommbomm34 <bommbomm34@perhof.org>
2026-02-17 20:29:56 +0100

    Add ClickableTable

---
+++ ui/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/gui/components/tables/ClickableTable.kt
@@ -0,0 +1,55 @@
+package io.github.bommbomm34.intervirt.gui.components.tables
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
+import io.github.windedge.table.DataTable
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ClickableTable(
+    headers: List<String>,
+    data: List<List<@Composable BoxScope.() -> Unit>>,
+    onClick: (Int) -> Unit // Parameter is the index of data list
+) {
+    val bg = MaterialTheme.colorScheme.onBackground
+    val scrollState = rememberScrollState()
+    val hoverData = data.map {
+        val interactionSource = remember { MutableInteractionSource() }
+        interactionSource to interactionSource.collectIsHoveredAsState()
+    }
+    DataTable(
+        columns = {
+            headerBackground {
+                Box(Modifier.background(bg))
+            }
+            headers.forEach {
+                column { VisibleText(it, true) }
+            }
+        },
+        modifier = Modifier.verticalScroll(scrollState)
+    ) {
+        data.forEachIndexed { i, composables ->
+            val hover = hoverData[i]
+            row(
+                Modifier
+                    .onClick { onClick(i) }
+                    .hoverable(hover.first)
+                    .background(if (hover.second.value) bg.copy(alpha = 0.5f) else bg)
+                    .pointerHoverIcon(PointerIcon.Hand)
+            ) {
+                composables.forEach {
+                    cell { it() }
+                }
+            }
+        }
+    }
+}
