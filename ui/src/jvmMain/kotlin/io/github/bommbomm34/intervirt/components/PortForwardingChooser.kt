97b7c268d57c0b5e3732ea1441c3f44c9d972b0d
bommbomm34 <bommbomm34@perhof.org>
2026-02-20 20:44:48 +0100

    Implement PortForwardingChooser

--- ui/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/components/PortForwardingChooser.kt
+++ ui/src/jvmMain/kotlin/io/github/bommbomm34/intervirt/components/PortForwardingChooser.kt
@@ -1,12 +1,38 @@
 package io.github.bommbomm34.intervirt.components
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import intervirt.ui.generated.resources.Res
+import intervirt.ui.generated.resources.external_port
+import intervirt.ui.generated.resources.internal_port
+import io.github.bommbomm34.intervirt.components.textfields.IntegerTextField
 import io.github.bommbomm34.intervirt.core.data.PortForwarding
+import org.jetbrains.compose.resources.stringResource
+
+private val PROTOCOLS = listOf("TCP", "UDP")
 
 @Composable
 fun PortForwardingChooser(
     portForwarding: PortForwarding,
     onChangePortForwarding: (PortForwarding) -> Unit,
 ) {
-    // TODO: Implement composable
+    var portForwarding by remember { mutableStateOf(PortForwarding("tcp", 0, 0)) }
+    CenterRow {
+        IntegerTextField(
+            value = portForwarding.hostPort,
+            onValueChange = { portForwarding = portForwarding.copy(hostPort = it) },
+            label = stringResource(Res.string.external_port),
+        )
+        GeneralSpacer()
+        IntegerTextField(
+            value = portForwarding.guestPort,
+            onValueChange = { portForwarding = portForwarding.copy(guestPort = it) },
+            label = stringResource(Res.string.internal_port),
+        )
+        GeneralSpacer()
+        SelectionDropdown(
+            options = PROTOCOLS,
+            selected = portForwarding.protocol.uppercase(),
+            onSelect = { portForwarding = portForwarding.copy(protocol = it.lowercase()) },
+        )
+    }
 }
