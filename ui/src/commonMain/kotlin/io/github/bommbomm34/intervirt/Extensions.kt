5bf452bbf0fef15641580d67d2d06ef912a99800
bommbomm34 <bommbomm34@perhof.org>
2026-02-21 17:20:17 +0100

    Organize Config.kt

---
+++ ui/src/commonMain/kotlin/io/github/bommbomm34/intervirt/Extensions.kt
@@ -0,0 +1,112 @@
+package io.github.bommbomm34.intervirt
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.PointerMatcher
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.input.pointer.PointerButton
+import androidx.compose.ui.platform.ClipEntry
+import androidx.compose.ui.platform.Clipboard
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import intervirt.ui.generated.resources.Res
+import io.github.bommbomm34.intervirt.components.CatchingLaunchedEffect
+import io.github.bommbomm34.intervirt.components.dialogs.ProgressDialog
+import io.github.bommbomm34.intervirt.core.api.DeviceManager
+import io.github.bommbomm34.intervirt.core.api.intervirtos.ProxyManager
+import io.github.bommbomm34.intervirt.core.api.intervirtos.general.DockerBasedManager
+import io.github.bommbomm34.intervirt.core.api.intervirtos.general.IntervirtOSClient
+import io.github.bommbomm34.intervirt.core.data.AppEnv
+import io.github.bommbomm34.intervirt.data.AppState
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.utils.io.CancellationException
+import org.koin.compose.koinInject
+import java.awt.datatransfer.StringSelection
+import java.net.ServerSocket
+
+fun String.versionCode() = replace(".", "").toInt()
+
+fun String.result() = Result.success(this)
+
+fun <T> Exception.result() = Result.failure<T>(this)
+
+@Composable
+fun dpToPx(dp: Dp) = with(LocalDensity.current) { dp.toPx() }
+
+suspend inline fun <T> runSuspendingCatching(block: suspend () -> T): Result<T> {
+    return try {
+        Result.success(block())
+    } catch (e: CancellationException) {
+        throw e
+    } catch (e: Throwable) {
+        Result.failure(e)
+    }
+}
+
+fun Int.isValidPort() = this in 1..65535
+
+fun Int.canPortBind(): Result<Unit> {
+    try {
+        ServerSocket(this).use {
+            return Result.success(Unit)
+        }
+    } catch (e: Exception) {
+        return Result.failure(e)
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+val PointerMatcher.Companion.Secondary: PointerMatcher
+    get() = PointerMatcher.mouse(PointerButton.Secondary)
+
+@Composable
+fun AppEnv.isDarkMode() = DARK_MODE ?: isSystemInDarkTheme()
+
+fun Dp.toPx() = density.run { toPx() }
+
+@Composable
+fun <T> IntervirtOSClient.rememberManager(func: (IntervirtOSClient) -> T): T = remember { func(this) }
+
+@Composable
+fun <T> IntervirtOSClient.rememberManager(func: (AppEnv, IntervirtOSClient) -> T): T {
+    val appEnv = koinInject<AppEnv>()
+    return remember { func(appEnv, this) }
+}
+
+
+@Composable
+fun rememberLogger(name: String) = remember { KotlinLogging.logger(name) }
+
+@Composable
+fun DockerBasedManager.initialize(): MutableState<Boolean> {
+    val appState = koinInject<AppState>()
+    val initialized = remember { mutableStateOf(false) }
+    CatchingLaunchedEffect {
+        appState.openDialog {
+            ProgressDialog(
+                flow = init(),
+                onClose = ::close,
+            )
+        }
+        initialized.value = true
+    }
+    return initialized
+}
+
+@Composable
+fun rememberProxyManager(
+    appEnv: AppEnv,
+    deviceManager: DeviceManager,
+    osClient: IntervirtOSClient,
+) = remember { ProxyManager(appEnv, deviceManager, osClient) }
+
+@OptIn(ExperimentalComposeUiApi::class)
+suspend fun Clipboard.copyToClipboard(text: String) {
+    setClipEntry(ClipEntry(StringSelection(text)))
+}
+
+internal suspend fun Res.readString(path: String) = readBytes(path).decodeToString()
