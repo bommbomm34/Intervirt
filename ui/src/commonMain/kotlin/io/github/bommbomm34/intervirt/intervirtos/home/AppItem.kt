6c83abd455bb3273c565765e18ce6095ed87db88
bommbomm34 <bommbomm34@perhof.org>
2026-02-23 19:05:21 +0100

    Use Cards in Home of IntervirtOS

--- ui/src/commonMain/kotlin/io/github/bommbomm34/intervirt/intervirtos/home/AppItem.kt
+++ ui/src/commonMain/kotlin/io/github/bommbomm34/intervirt/intervirtos/home/AppItem.kt
@@ -1,12 +1,10 @@
 package io.github.bommbomm34.intervirt.intervirtos.home
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.onClick
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.github.bommbomm34.intervirt.components.GeneralIcon
@@ -14,23 +12,22 @@ import io.github.bommbomm34.intervirt.components.GeneralSpacer
 import org.jetbrains.compose.resources.stringResource
 
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun AppItem(
     appInfo: AppInfo,
     onClick: (AppInfo) -> Unit,
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .padding(16.dp)
-            .onClick(onClick = { onClick(appInfo) }),
+    Card(
+        modifier = Modifier.padding(16.dp),
+        onClick = { onClick(appInfo) },
     ) {
-        GeneralIcon(
-            imageVector = appInfo.icon,
-            contentDescription = stringResource(appInfo.description),
-        )
-        GeneralSpacer(2.dp)
-        Text(stringResource(appInfo.name))
+        Column(Modifier.padding(16.dp)) {
+            GeneralIcon(
+                imageVector = appInfo.icon,
+                contentDescription = stringResource(appInfo.description),
+            )
+            GeneralSpacer(2.dp)
+            Text(stringResource(appInfo.name))
+        }
     }
 }
