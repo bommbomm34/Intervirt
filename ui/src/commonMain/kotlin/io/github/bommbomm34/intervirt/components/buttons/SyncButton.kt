f54cb3530d332168da05af88998d188bb1c19215
bommbomm34 <bommbomm34@perhof.org>
2026-02-23 19:31:44 +0100

    Improve SyncButton and ProgressDialog

--- ui/src/commonMain/kotlin/io/github/bommbomm34/intervirt/components/buttons/SyncButton.kt
+++ ui/src/commonMain/kotlin/io/github/bommbomm34/intervirt/components/buttons/SyncButton.kt
@@ -9,12 +9,12 @@ import androidx.compose.runtime.*
 import intervirt.ui.generated.resources.Res
 import intervirt.ui.generated.resources.sync_guest
 import intervirt.ui.generated.resources.syncing
+import io.github.bommbomm34.intervirt.components.dialogs.ProgressDialog
 import io.github.bommbomm34.intervirt.core.api.GuestManager
+import io.github.bommbomm34.intervirt.core.data.AppEnv
 import io.github.bommbomm34.intervirt.core.data.IntervirtConfiguration
 import io.github.bommbomm34.intervirt.core.data.ResultProgress
 import io.github.bommbomm34.intervirt.data.AppState
-import io.github.bommbomm34.intervirt.data.Severity
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
@@ -22,28 +22,26 @@ import org.koin.compose.koinInject
 @Composable
 fun SyncButton(running: Boolean) {
     val appState = koinInject<AppState>()
+    val appEnv = koinInject<AppEnv>()
     val guestManager = koinInject<GuestManager>()
     val configuration = koinInject<IntervirtConfiguration>()
     val scope = rememberCoroutineScope()
     var syncing by remember { mutableStateOf(false) }
     var syncFailed by remember { mutableStateOf(false) }
-    if (running) {
+    if (running || appEnv.VIRTUAL_AGENT_MODE) {
         IconButton(
             onClick = {
                 scope.launch {
                     syncing = true
-                    configuration.syncConfiguration(guestManager).collect {
-                        syncFailed = it is ResultProgress.Result && it.result.isFailure
-                        appState.logs.add(it.log())
-                        if (syncFailed) {
-                            appState.openDialog(
-                                severity = Severity.ERROR,
-                                message = it.log(),
-                            )
-                            coroutineContext.cancel()
-                        }
+                    appState.openDialog {
+                        ProgressDialog(
+                            flow = configuration.syncConfiguration(guestManager),
+                            onMessage = {
+                                if (it is ResultProgress.Result) syncing = false
+                            },
+                            onClose = ::close,
+                        )
                     }
-                    syncing = false
                 }
             },
             enabled = !syncing,
--- ui/src/commonMain/kotlin/io/github/bommbomm34/intervirt/components/dialogs/ProgressDialog.kt
+++ ui/src/commonMain/kotlin/io/github/bommbomm34/intervirt/components/dialogs/ProgressDialog.kt
@@ -1,20 +1,10 @@
 package io.github.bommbomm34.intervirt.components.dialogs
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import intervirt.ui.generated.resources.Res
-import intervirt.ui.generated.resources.cancel
-import io.github.bommbomm34.intervirt.components.AlignedBox
+import androidx.compose.runtime.Composable
 import io.github.bommbomm34.intervirt.components.CenterColumn
 import io.github.bommbomm34.intervirt.components.FlowProgressView
-import io.github.bommbomm34.intervirt.components.GeneralSpacer
-import io.github.bommbomm34.intervirt.components.buttons.CloseButton
 import io.github.bommbomm34.intervirt.core.data.ResultProgress
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
-import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun <T> ProgressDialog(
@@ -22,22 +12,7 @@ fun <T> ProgressDialog(
     onMessage: ((ResultProgress<T>) -> Unit)? = null,
     onClose: () -> Unit,
 ) {
-    var job: Job? by remember { mutableStateOf(null) }
-    AlignedBox(Alignment.TopStart) {
-        CloseButton(onClose)
-    }
     CenterColumn {
-        FlowProgressView(flow, { job = it }, onMessage)
-        GeneralSpacer()
-        job?.let {
-            Button(
-                onClick = {
-                    it.cancel()
-                    onClose()
-                },
-            ) {
-                Text(stringResource(Res.string.cancel))
-            }
-        }
+        FlowProgressView(flow, { if (it == null) onClose() }, onMessage)
     }
 }
