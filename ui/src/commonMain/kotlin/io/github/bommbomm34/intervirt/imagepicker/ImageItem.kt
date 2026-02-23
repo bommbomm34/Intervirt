d40ba36b4e300b2186b1b44f111e9b95590b7f89
bommbomm34 <bommbomm34@perhof.org>
2026-02-23 19:03:07 +0100

    Use Cards in ImagePicker

--- ui/src/commonMain/kotlin/io/github/bommbomm34/intervirt/imagepicker/ImageItem.kt
+++ ui/src/commonMain/kotlin/io/github/bommbomm34/intervirt/imagepicker/ImageItem.kt
@@ -1,25 +1,24 @@
 package io.github.bommbomm34.intervirt.imagepicker
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.onClick
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.github.bommbomm34.intervirt.components.GeneralSpacer
 import io.github.bommbomm34.intervirt.data.Image
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ImageItem(image: Image, onShowImage: () -> Unit) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.onClick(onClick = onShowImage),
+    Card(
+        onClick = onShowImage,
     ) {
-        ImageIcon(image)
-        GeneralSpacer(2.dp)
-        Text(image.toReadableName())
+        Column(Modifier.padding(16.dp)){
+            ImageIcon(image)
+            GeneralSpacer(2.dp)
+            Text(image.toReadableName())
+        }
     }
 }
