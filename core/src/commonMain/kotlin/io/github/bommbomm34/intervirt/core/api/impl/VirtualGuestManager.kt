5c35bad72d66615ea272dc948983f1ba45fd64c7
bommbomm34 <bommbomm34@perhof.org>
2026-02-23 19:35:02 +0100

    Add artificial delays to VirtualGuestManager

--- core/src/commonMain/kotlin/io/github/bommbomm34/intervirt/core/api/impl/VirtualGuestManager.kt
+++ core/src/commonMain/kotlin/io/github/bommbomm34/intervirt/core/api/impl/VirtualGuestManager.kt
@@ -8,6 +8,8 @@ import io.github.bommbomm34.intervirt.core.exceptions.NotFoundException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
+private const val DELAY = 500L
+
 // Virtual Guest Manager
 class VirtualGuestManager : GuestManager {
     private val containers = mutableListOf<Container>()
@@ -21,24 +23,29 @@ class VirtualGuestManager : GuestManager {
         internet: Boolean,
         image: String,
     ): Result<Unit> {
+        delay()
         containers.add(Container(id, initialIpv4, initialIpv6, mac, internet, image))
         return Result.success(Unit)
     }
 
     override suspend fun removeContainer(id: String): Result<Unit> {
+        delay()
         val removed = containers.removeIf { it.id == id }
         return if (removed) Result.success(Unit) else Result.failure(NotFoundException("Container $id not found."))
     }
 
     override suspend fun setIpv4(id: String, newIP: String): Result<Unit> = runCatching {
+        delay()
         containers.first { it.id == id }.ipv4 = newIP
     }
 
     override suspend fun setIpv6(id: String, newIP: String): Result<Unit> = runCatching {
+        delay()
         containers.first { it.id == id }.ipv6 = newIP
     }
 
     override suspend fun connect(id1: String, id2: String): Result<Unit> {
+        delay()
         if (!id1.exists()) return Result.failure(NotFoundException("Container $id1 doesn't exist."))
         if (!id2.exists()) return Result.failure(NotFoundException("Container $id2 doesn't exist."))
         connections.add(ContainerConnection(id1, id2))
@@ -46,6 +53,7 @@ class VirtualGuestManager : GuestManager {
     }
 
     override suspend fun disconnect(id1: String, id2: String): Result<Unit> {
+        delay()
         if (!id1.exists()) return Result.failure(NotFoundException("Container $id1 doesn't exist."))
         if (!id2.exists()) return Result.failure(NotFoundException("Container $id2 doesn't exist."))
         connections.remove(ContainerConnection(id1, id2))
@@ -53,6 +61,7 @@ class VirtualGuestManager : GuestManager {
     }
 
     override suspend fun setInternetAccess(id: String, enabled: Boolean): Result<Unit> = runCatching {
+        delay()
         getContainerByID(id).internet = enabled
     }
 
@@ -62,6 +71,7 @@ class VirtualGuestManager : GuestManager {
         externalPort: Int,
         protocol: String,
     ): Result<Unit> = runCatching {
+        delay()
         getContainerByID(id).portForwardings.add(
             PortForwarding(
                 protocol = protocol,
@@ -75,28 +85,34 @@ class VirtualGuestManager : GuestManager {
         externalPort: Int,
         protocol: String,
     ): Result<Unit> = runCatching {
+        delay()
         containers.forEach { container ->
             container.portForwardings.removeIf { it.externalPort == externalPort && it.protocol == protocol }
         }
     }
 
     override suspend fun startContainer(id: String): Result<Unit> = runCatching {
+        delay()
         getContainerByID(id).running = true
     }
 
     override suspend fun stopContainer(id: String): Result<Unit> = runCatching {
+        delay()
         getContainerByID(id).running = false
     }
 
     override fun wipe(): Flow<ResultProgress<Unit>> = flow {
+        delay()
         emit(ResultProgress.proceed(0.2f, "Deleting containers..."))
         containers.clear()
+        delay()
         emit(ResultProgress.proceed(0.5f, "Deleting connections..."))
         connections.clear()
         emit(ResultProgress.success(Unit))
     }
 
     override fun update(): Flow<ResultProgress<Unit>> = flow {
+        delay()
         emit(ResultProgress.success(Unit))
     }
 
@@ -110,6 +126,8 @@ class VirtualGuestManager : GuestManager {
     private fun getContainerByID(id: String) = containers.first { it.id == id }
 
     private fun String.exists() = containers.any { it.id == this }
+
+    private suspend fun delay() = kotlinx.coroutines.delay(DELAY)
 
     override suspend fun close() = Result.success(Unit) // Nothing to close
 }
