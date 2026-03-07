6f9f4eb82a7ab1ef0c7b41dd6fb4d3ea0a6d7201
bommbomm34 <bommbomm34@perhof.org>
2026-03-07 23:01:24 +0100

    Add ExecutorTest

--- core/src/commonMain/kotlin/io/github/bommbomm34/intervirt/core/api/Executor.kt
+++ core/src/commonMain/kotlin/io/github/bommbomm34/intervirt/core/api/Executor.kt
@@ -12,6 +12,7 @@ import io.github.bommbomm34.intervirt.core.util.getLogger
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import java.io.File
@@ -21,7 +22,6 @@ class Executor(appEnv: AppEnv) {
 
     fun runCommandOnHost(workingFolder: File?, commands: List<String>): Flow<CommandStatus> =
         flow {
-
             val builder = ProcessBuilder(commands)
             workingFolder?.let { builder.directory(it) }
             builder.redirectErrorStream()
@@ -36,5 +36,10 @@ class Executor(appEnv: AppEnv) {
             val statusCode = process.waitFor()
             logger.debug { "Process ended with status code $statusCode" }
             emit(statusCode.toCommandStatus())
-        }.flowOn(Dispatchers.IO)
+        }
+            .flowOn(Dispatchers.IO)
+            .catch {
+                emit(it.localizedMessage.toCommandStatus())
+                emit(1.toCommandStatus())
+            }
 }
---
+++ core/src/commonTest/kotlin/io/github/bommbomm34/intervirt/core/api/ExecutorTest.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2026. Intervirt Contributors
+ * Licensed under the GNU General Public License 3.
+ */
+
+package io.github.bommbomm34.intervirt.core.api
+
+import io.github.bommbomm34.intervirt.core.data.OS
+import io.github.bommbomm34.intervirt.core.data.getCommandResult
+import io.github.bommbomm34.intervirt.core.data.getOS
+import kotlinx.coroutines.test.runTest
+import java.io.File
+import java.util.UUID
+import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+class ExecutorTest {
+    val executor = Executor(getTestAppEnv())
+
+    @Test
+    fun shouldRunSuccessfulCommandOnHost() = runTest {
+        val (output, status) = executor
+            .runCommandOnHost(null, listOf(*getEchoPath(), "Hello World"))
+            .getCommandResult()
+        assertEquals(0, status)
+        assertContains(output, "Hello World")
+    }
+
+    @Test
+    fun shouldRunSuccessfulCommandOnHostWithWorkingFolder() = runTest {
+        val testFolder = when (getOS()){
+            OS.WINDOWS -> File("C:\\Windows\\System32\\drivers\\etc\\")
+            OS.LINUX -> File("/etc/")
+        }
+
+        val (_, status) = executor
+            .runCommandOnHost(testFolder, listOf(*getCatPath(), "hosts"))
+            .getCommandResult()
+        assertEquals(0, status)
+    }
+
+    @Test
+    fun shouldRunNotExistingCommandOnHost() = runTest {
+        val (_, status) = executor
+            .runCommandOnHost(null, listOf("invalid_command_intervirt_${UUID.randomUUID().hashCode()}"))
+            .getCommandResult()
+        assertNotEquals(0, status)
+    }
+
+    private fun getCatPath(): Array<String> = when (getOS()){
+        OS.WINDOWS -> arrayOf("C:\\Windows\\System32\\cmd.exe", "type")
+        OS.LINUX -> arrayOf("/usr/bin/cat")
+    }
+
+    private fun getEchoPath(): Array<String> = when (getOS()){
+        OS.WINDOWS -> arrayOf("C:\\Windows\\System32\\cmd.exe", "echo")
+        OS.LINUX -> arrayOf("/usr/bin/echo")
+    }
+}
